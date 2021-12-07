26580779d050c251c957747bc84130bfe78fcb40
AHEMD-cmd <78764410+AHEMD-cmd@users.noreply.github.com>
2021-12-07 17:16:07 +0200

    second

---
+++ resources/views/show.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE HTML>
+<!--
+	Solid State by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Solid State by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <base href="/public">
+
+        <link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">Solid State</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+                            <ul class="links" >
+								<li><a href="/">Home</a></li>
+                                @auth
+
+                                @if (auth()->user()->type == 'admin')
+
+								<li><a href="{{route('contact.index')}}">Contact</a></li>
+								<li><a href="{{route('project.index')}}">Projects</a></li>
+								<li><a href="{{route('site.edit',$site->id)}}">Edit site setting</a></li>
+                                @endif
+                                @endauth
+                                @guest
+                                <li><a href="{{route('login')}}">Log In</a></li>
+                                <li><a href="{{route('register')}}">Sign Up</a></li>
+                                @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="post" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+
+                            @endguest
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+
+
+
+
+						<!-- Four -->
+							<section id="four" class="wrapper alt style1">
+								<div class="inner">
+									<h2 class="major">show page</h2>
+									<section class="features">
+										<article>
+											<a href="#" class="image"><img src="{{'cover/'. $project->cover }}" height="300px" /></a>
+
+                                            @foreach ($project->images as $img)
+                                            <a href="#" class="image"><img src="{{'images/'. $img->image }}" height="300px" /></a>
+                                            @endforeach
+
+										</article>
+
+                                        <br>
+									</section>
+
+
+
+								</div>
+
+
+							</section>
+
+					</section>
+
+				
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
