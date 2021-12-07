c3c0357dce330eb74114306149e644d55a530344
AHEMD-cmd <78764410+AHEMD-cmd@users.noreply.github.com>
2021-12-07 14:01:13 +0200

    second

--- database/seeds/SiteSeeder.php
+++ database/seeds/SiteSeeder.php
@@ -16,7 +16,7 @@ class SiteSeeder extends Seeder
             'title1' => 'Amira Elsayed Innovation',
             'sub1' => 'choose how can be your future home',
             'about_us' => 'about me',
-            'sub2' => "Amira Elsayed, Egyptian Interior and Furniture Designer, I gained Bachelor's Degree in Interior Design In 2020, I Obsessed with Interior Design, I am very Lucky to Work In the Same Field that I love, I Worked In this Field Since 2018 until Today. I Started by establishing my Page in 2018.I Started my Career from Home As Freelancer Designer, Designing Apartments, Shops, Hotels and Not Only this, I excelled in Designing Unique Pieces of furniture with Innovative Ideas. ",
+            'sub2' => "Amira Elsayed, Egyptian Interior and Furniture Designer, I gained Bachelor's Degree in Interior Design In 2020, I Obsessed with Interior Design, I am very Lucky to Work In the Same Field that I love, I Worked In this Field Since 2018 until Today. I Started by establishing my Page in 2018.  ",
             'image' => '1638797870_264165542_277440861014397_1882457108722520349_n.jpg',
         ]);
     }
