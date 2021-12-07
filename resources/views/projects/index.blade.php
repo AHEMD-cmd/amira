8d82a5ceef1a5f56e44c44cda5c0cfa89c27633d
AHEMD-cmd <78764410+AHEMD-cmd@users.noreply.github.com>
2021-12-07 19:32:02 +0200

    second

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -90,7 +90,7 @@
 											<a href="{{route('project.show', $project->id)}}" class="image"><img src="{{'cover/'. $project->cover }}" height="300px" /></a>
 											<h3 class="major">{{$project->title}}</h3>
 											<p>{{$project->desc}}.</p>
-											<a href="{{route('project.show', $project->id)}}" class="special">view project</a>
+											<a href="{{route('project.show', $project->id)}}" class="special" style="display: inline">view  </a>
                                             <a href="{{route('project.edit', $project->id)}}" class="special" style="display: inline">Edit</a>
                                             <a href="{{route('project.destroy', $project->id)}}" class="special" style="display: inline;position: relative;left:20px">Delete</a>
 										</article>
@@ -109,65 +109,6 @@
 
 					</section>
 
-				<!-- Footer -->
-					<section id="footer">
-						<div class="inner">
-							<h2 class="major">Get in touch</h2>
-							<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
-							<form method="post" action="{{route('contact.store')}}" enctype="multipart/form-data">
-                                @csrf
-								<div class="fields">
-									<div class="field">
-										<label for="name">Name</label>
-										<input type="text" name="name" id="name" class=" @error('name') is-invalid @enderror" />
-                                        @error('name')
-                                        <div  class="invalid-feedback">{{$message}}</div>
-                                        @enderror
-                                    </div>
-									<div class="field">
-										<label for="email">Email</label>
-										<input type="email" name="email" id="email"  class=" @error('email') is-invalid @enderror" />
-                                        @error('email')
-                                        <div  class="invalid-feedback">{{$message}}</div>
-                                        @enderror
-                                    </div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="4" class=" @error('email') is-invalid @enderror"></textarea>
-                                        @error('message')
-                                        <div  class="invalid-feedback">{{$message}}</div>
-                                        @enderror
-                                    </div>
-									<div class="field">
-										<label for="Image">Image</label>
-										<input type="file" name="image" id="Image" rows="4" class=" @error('image') is-invalid @enderror">
-                                        <small style="font-size: 14px; color:rgb(144, 184, 72)">image is optional</small>
-                                        @error('image')
-                                        <div  class="invalid-feedback">{{$message}}</div>
-                                        @enderror
-                                    </div>
-								</div>
-								<ul class="actions">
-									<li><input type="submit" value="Send Message" /></li>
-								</ul>
-							</form>
-							<ul class="contact">
-								<li class="icon solid fa-home">
-									Egypt<br />
-									Al-sharqia<br />
-									Minya al-Qamh
-								</li>
-								<li class="icon solid fa-phone">01022953646</li>
-								{{-- <li class="icon solid fa-envelope"><a href="Amiraelsayed9090@gmail.com">Amiraelsayed9090@gmail.com</a></li> --}}
-								<li class="icon brands fa-behance"><a href="https://www.behance.net/amiraelsayb861">https://www.behance.net/amiraelsayb861</a></li>
-								<li class="icon brands fa-facebook-f"><a href="https://www.facebook.com/HomeCurve">https://www.facebook.com/HomeCurve</a></li>
-								<li class="icon brands fa-instagram"><a href="https://www.instagram.com/amira.elsayed.innovation/">https://www.instagram.com/amira.elsayed.innovation/</a></li>
-							</ul>
-							<ul class="copyright">
-								<li>&copy; Untitled Inc. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a>&nbsp; &nbsp;&nbsp; Made by Ahmed Yasser  &nbsp; &nbsp;&nbsp;  Phone : 01011017982</li>
-							</ul>
-						</div>
-					</section>
 
 			</div>
 
