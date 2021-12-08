b1f391b4f6d5d75bf74ab71d25b04cb46c8b78c9
AHEMD-cmd <78764410+AHEMD-cmd@users.noreply.github.com>
2021-12-08 18:48:57 +0200

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -104,7 +104,7 @@
 						<!-- One -->
                         <section  class="wrapper alt  spotlight style2"  >
                             <div class="inner">
-                                <a href="#" class="image"><img src="https://i.pinimg.com/564x/e4/f9/67/e4f9676d8b701ce5750775c7d8bef985.jpg" width="260px" height="260px" style="border-radius: 50%" /></a>
+                                <a href="#" class="image"><img src="{{'images/we.jpg'}}" width="260px" height="260px" style="border-radius: 50%" /></a>
                                 <div class="content">
                                     <h2 class="major">what we do </h2>
                                     <p>We are Here to Design your Furniture and your space (Residential, Administrative,Commercial,Tourism)</p>
